0e9ba9aa9cae350dff5895d235f357427b8bbd92
Tonie Crumley <tonie.crumley@gmail.com>
2024-06-25 08:38:26 -0500

    Add splice junction plots
    Splice junction plots were added along with dynamic UI interactions
    for selecting gene and splice junction.  Also dynamic showning and
    hiding of plots based on user selections.

--- server.R
+++ server.R
@@ -1,12 +1,41 @@
 library(shiny)
+library(shinyjs)
 library(shinycssloaders)
 library(MARVEL)
 library(tidyverse)
 
 server <- function(input, output, session) {
+  # Load data and split into wildtype and mutant.
   setbp1 <- reactiveVal(readRDS("./data/setbp1_marvel_aligned_reduced_size.rds"))
 
+  wildtype_setbp1 <- reactive({
+    data <- setbp1()
+    data$sample.metadata <- data$sample.metadata %>%
+      filter(seq_folder == 'wildtype')
+    data$pca <- data$pca[data$pca$cell.id %in% data$sample.metadata$cell.id, ]
+    data$gene.norm.matrix <- data$gene.norm.matrix[, data$sample.metadata$cell.id]
+    data$gene.count.matrix <- data$gene.count.matrix[, data$sample.metadata$cell.id]
+    data$sj.count.matrix <- data$sj.count.matrix[, data$sample.metadata$cell.id]
+    data
+  })
+
+  mutant_setbp1 <- reactive({
+    data <- setbp1()
+    data$sample.metadata <- data$sample.metadata %>%
+      filter(seq_folder == 'mutant')
+    data$pca <- data$pca[data$pca$cell.id %in% data$sample.metadata$cell.id, ]
+    data$gene.norm.matrix <- data$gene.norm.matrix[, data$sample.metadata$cell.id]
+    data$gene.count.matrix <- data$gene.count.matrix[, data$sample.metadata$cell.id]
+    data$sj.count.matrix <- data$sj.count.matrix[, data$sample.metadata$cell.id]
+    data
+  })
+
+  # Setup the gene and splice junction selectors.
   gene_list <- reactive(c(c(""), setbp1()$gene.metadata$gene_short_name %>% sort()))
+  splice_junction_list <- reactive({
+    splice_junctions <- setbp1()$sj.metadata %>% filter(gene_short_name.start == input$gene)
+    c(c(""), splice_junctions$coord.intron %>% sort())
+  })
 
   observe({
     updateSelectizeInput(
@@ -20,6 +49,42 @@ server <- function(input, output, session) {
     )
   })
 
+  observeEvent(input$gene, {
+    if (input$gene == "") {
+      shinyjs::hide("splice_junction")
+      shinyjs::hide("geneExpressionPlots")
+      shinyjs::hide("spliceJunctionPlots")
+      updateSelectizeInput(
+        session,
+        "splice_junction",
+        choices = c(""),
+        options = list(
+          placeholder = "Loading..."
+        )
+      )
+    } else {
+      updateSelectizeInput(
+        session,
+        "splice_junction",
+        choices = splice_junction_list(),
+        options = list(
+          placeholder = "Select a splice junction"
+        )
+      )
+      shinyjs::show("splice_junction")
+      shinyjs::show("geneExpressionPlots")
+    }
+  })
+
+  observeEvent(input$splice_junction, {
+    if (input$splice_junction == "") {
+      shinyjs::hide("spliceJunctionPlots")
+    } else {
+      shinyjs::show("spliceJunctionPlots")
+    }
+  })
+
+  # Create the plots.
   output$cellTypePlot <- renderPlot({
     # The following cell_group_list code is based on code authored by Emma Jones.
     # 230926_EJ_Setbp1_AlternativeSplicing/src/marvel/03_analyze_de_genes.Rmd
@@ -46,19 +111,10 @@ server <- function(input, output, session) {
     plot
   })
 
-  wildtype_gene_expression <- reactive({
-    data <- setbp1()
-    data$sample.metadata <- data$sample.metadata %>%
-      filter(seq_folder == 'wildtype')
-    data$pca <- data$pca[data$pca$cell.id %in% data$sample.metadata$cell.id, ]
-    data$gene.norm.matrix <- data$gene.norm.matrix[, data$sample.metadata$cell.id]
-    data
-  })
-
   output$wildtypeGeneExpressionPlot <- renderPlot({
     if (input$gene != "") {
       plot <- PlotValues.PCA.Gene.10x(
-        MarvelObject=wildtype_gene_expression(),
+        MarvelObject=wildtype_setbp1(),
         gene_short_name=input$gene,
         color.gradient=c("grey","cyan","green","yellow","red"),
         type="umap"
@@ -69,25 +125,48 @@ server <- function(input, output, session) {
     }
   })
 
-  mutant_gene_expression <- reactive({
-    data <- setbp1()
-    data$sample.metadata <- data$sample.metadata %>%
-      filter(seq_folder == 'mutant')
-    data$pca <- data$pca[data$pca$cell.id %in% data$sample.metadata$cell.id, ]
-    data$gene.norm.matrix <- data$gene.norm.matrix[, data$sample.metadata$cell.id]
-    data
-  })
-
   output$mutantGeneExpressionPlot <- renderPlot({
     if (input$gene != "") {
       plot <- PlotValues.PCA.Gene.10x(
-        MarvelObject=mutant_gene_expression(),
+        MarvelObject=mutant_setbp1(),
         gene_short_name=input$gene,
         color.gradient=c("grey","cyan","green","yellow","red"),
         type="umap"
       )
       plot$adhocPlot$PCA$Gene <- plot$adhocPlot$PCA$Gene +
         labs(title = paste("Mutant Gene Expression for", input$gene))
+      plot
+    }
+  })
+
+  output$wildtypeSpliceJunctionPlot <- renderPlot({
+    if (input$splice_junction != "") {
+      plot <- PlotValues.PCA.PSI.10x(
+        MarvelObject=wildtype_setbp1(),
+        coord.intron=input$splice_junction,
+        min.gene.count=3,
+        log2.transform=FALSE,
+        color.gradient=c("grey","cyan","green","yellow","red"),
+        type="umap"
+      )
+      plot$adhocPlot$PCA$PSI <- plot$adhocPlot$PCA$PSI +
+        labs(title = paste("Wildtype Splice Junction Usage for", input$splice_junction))
+      plot
+    }
+  })
+
+  output$mutantSpliceJunctionPlot <- renderPlot({
+    if (input$splice_junction != "") {
+      plot <- PlotValues.PCA.PSI.10x(
+        MarvelObject=mutant_setbp1(),
+        coord.intron=input$splice_junction,
+        min.gene.count=3,
+        log2.transform=FALSE,
+        color.gradient=c("grey","cyan","green","yellow","red"),
+        type="umap"
+      )
+      plot$adhocPlot$PCA$PSI <- plot$adhocPlot$PCA$PSI +
+        labs(title = paste("Mutant Splice Junction Usage for", input$splice_junction))
       plot
     }
   })
--- ui.R
+++ ui.R
@@ -1,24 +1,75 @@
-ui <- fluidPage(
-    titlePanel("Alternative Splicing in Schinzel-Giedion Syndrome"),
+library(shiny)
+library(shinyjs)
+library(shinycssloaders)
 
-    sidebarLayout(
-        sidebarPanel(
-          selectizeInput(
-            "gene",
-            "Gene",
-            choices = NULL,
-            options = list(
-              maxOptions = 5,
-              maxItems = 1,
-              placeholder = "Loading..."
-            )
+ui <- fluidPage(
+  useShinyjs(),
+
+  titlePanel("Alternative Splicing in Schinzel-Giedion Syndrome"),
+
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(
+        "gene",
+        "Gene",
+        choices = NULL,
+        options = list(
+          maxOptions = 5,
+          maxItems = 1,
+          placeholder = "Loading..."
+        )
+      ),
+      shinyjs::hidden(
+        selectizeInput(
+          "splice_junction",
+          "Splice Junction",
+          choices = NULL,
+          options = list(
+            maxItems = 1,
+            placeholder = "Loading..."
+          )
+        )
+      )
+    ),
+
+    mainPanel(
+      shinycssloaders::withSpinner(plotOutput("cellTypePlot"))
+    )
+  ),
+  shinyjs::hidden(
+    div(id = "geneExpressionPlots",
+      fluidRow(
+        column(
+          6,
+          shinycssloaders::withSpinner(
+            plotOutput("wildtypeGeneExpressionPlot")
+          ),
+        ),
+        column(
+          6,
+          shinycssloaders::withSpinner(
+            plotOutput("mutantGeneExpressionPlot")
+          )
+        )
+      )
+    )
+  ),
+  shinyjs::hidden(
+    div(id = "spliceJunctionPlots",
+      fluidRow(
+        column(
+          6,
+          shinycssloaders::withSpinner(
+            plotOutput("wildtypeSpliceJunctionPlot")
           )
         ),
-
-        mainPanel(
-          shinycssloaders::withSpinner(plotOutput("cellTypePlot")),
-          shinycssloaders::withSpinner(plotOutput("wildtypeGeneExpressionPlot")),
-          shinycssloaders::withSpinner(plotOutput("mutantGeneExpressionPlot"))
+        column(
+          6,
+          shinycssloaders::withSpinner(
+            plotOutput("mutantSpliceJunctionPlot")
+          )
         )
+      )
     )
+  )
 )
